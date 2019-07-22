24f6d177cdecda7a8a4807477132de9677e65e46
EnzoRobaina <enzo.robaina@gmail.com>
2019-07-22 10:54:36 -0300

    Adiciona polyline e funcão isOnEdge

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 function initMap(){
+    const polyArray = [
+        {lat:-21.7537901,lng:-41.3252839},
+        {lat:-21.7540147,lng:-41.3253784},
+        {lat:-21.7541181,lng:-41.3254227},
+        {lat:-21.7542171,lng:-41.3254645},
+        {lat:-21.7543619,lng:-41.3255252},
+        {lat:-21.7545254,lng:-41.3255952},
+        {lat:-21.7547471,lng:-41.3256888},
+        {lat:-21.7546973,lng:-41.3258108},
+        {lat:-21.7546639,lng:-41.3258869},
+        {lat:-21.7546319,lng:-41.325961},
+        {lat:-21.7547321,lng:-41.3260086}]
+    
+    
     const CAMPOS = new google.maps.LatLng(-21.759757, -41.322010)
     
     let map = new google.maps.Map(document.getElementById('map'), {
@@ -9,15 +23,24 @@ function initMap(){
     let marker1 = new google.maps.Marker({
         map: map,
         draggable: false,
-        position: {lat: -21.756801, lng: -41.318911},
+        position: {lat:-21.7537901,lng:-41.3252839},
         title: '1'
     })
 
     let marker2 = new google.maps.Marker({
         map: map,
         draggable: true,
-        position: {lat: -21.756801, lng: -41.4},
+        position: {lat: -21.756801, lng: -41.318911},
         title: '2'
+    })
+
+    let poly = new google.maps.Polyline({
+        path: polyArray,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+        map: map
     })
 
     marker2.addListener('dragend', (event)=>{
@@ -25,6 +48,7 @@ function initMap(){
         let coordinates2 = new google.maps.LatLng(marker2.getPosition().lat(), marker2.getPosition().lng())
         console.log(google.maps.geometry.spherical.computeDistanceBetween(marker1.getPosition(), marker2.getPosition()).toFixed(2) + 'm')
         console.log(haversineFormula(coordinates1, coordinates2))
+        console.log(`is on edge? ${google.maps.geometry.poly.isLocationOnEdge(marker2.getPosition(), poly, 20e-4)}`)
     })
 
     function toRadians(value) {
